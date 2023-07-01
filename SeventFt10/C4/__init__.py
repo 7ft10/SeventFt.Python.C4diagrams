7b1973ac53cf1b9925b135d1577a8aaf8bb4f33f
Mike Burns <mike@7ft10.com>
2023-07-01 21:50:54 +1000

    imports

--- SeventFt10/C4/__init__.py
+++ SeventFt10/C4/__init__.py
@@ -1,2 +1,4 @@
 # SevenFt10/C4/__init__.py
-from . import Diagrams, Factory, FormatterMixIn, Repository
+from .Diagrams import C4Node, Container, Persona, Database, System, Component, Code, System
+from .Factory import Factory
+from .Repository import Repository
--- SeventFt10/__init__.py
+++ SeventFt10/__init__.py
@@ -1,4 +1,2 @@
 # SevenFt10/__init__.py
-from C4.Diagrams import C4Node, Container, Persona, Database, System, Component, Code, System
-from C4.Factory import Factory
-from C4.Repository import Repository
+from . import C4
