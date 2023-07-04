bf7137f0fc1d67539a57325ac6f8641893d776b4
Mike Burns <mike@7ft10.com>
2023-07-04 11:36:26 +1000

    test run all

--- tests/test_load.py
+++ tests/test_load.py
@@ -4,4 +4,7 @@ def test_load():
     Repo = Repository.LoadFromUrl('https://raw.githubusercontent.com/7ft10/C4ArchitectureExamples/main/Repository/', module_name = "Repository")
     Repo.Personas.BankingCustomer.Print()
 
-test_load()
+def run_all():
+	test_load()
+
+run_all()
